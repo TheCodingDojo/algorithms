4e99e1587e0f20e124898678d5ea44facbd8fad4
Neil M <neilm813@gmail.com>
2021-03-30 16:02:50 -0700

    fix missing breaking test cases

--- spec/strings/decodeStrSpec.js
+++ spec/strings/decodeStrSpec.js
@@ -5,7 +5,13 @@ describe("decodeStr", () => {
   const str1 = "a3b2c1d3";
   const expected1 = "aaabbcddd";
 
-  const testCases = [{ arguments: [str1], expected: expected1 }];
+  const str2 = "a3b2c12d10";
+  const expected2 = "aaabbccccccccccccdddddddddd";
+
+  const testCases = [
+    { arguments: [str1], expected: expected1 },
+    { arguments: [str2], expected: expected2 },
+  ];
 
   it("should return a decoded version of the given string such that each char is repeated based on integer that follows.", () =>
     testCases.forEach(({ arguments, expected }) =>
--- strings/decodeStr.js
+++ strings/decodeStr.js
@@ -1,10 +1,12 @@
 /* 
-TODO UPDATE FOR LONGER INTS
   String Decode  
 */
 
 const str1 = "a3b2c1d3";
 const expected1 = "aaabbcddd";
+
+const str2 = "a3b2c12d10";
+const expected2 = "aaabbccccccccccccdddddddddd";
 
 /**
  * Decodes the given string by duplicating each character by the following int
@@ -22,19 +24,62 @@ module.exports = { decodeStr };
 /*****************************************************************************/
 
 /**
- * - Time: O(n * m), where n = str.length and m = largest (worst case) int in the
- *    string which will nested loop from .repeat.
- * - Space: O(n * m).
+ * Decodes the given string by duplicating each character by the following int
+ * amount if there is an int after the character.
+ * - Time: O(n) linear. The nested loop increments i as well, so every
+ *    nested iteration gets us closer to finishing the outer loop which at most
+ *    amounts to looping over the full string.
+ * - Space: Worst case is max string size is reached because we don't know how
+ *    large the given ints will be.
+ * @param {string} str An encoded string with characters that may have an int
+ *    after indicating how many times the character occurs.
+ * @returns {string} The given str decoded / expanded.
  */
 function decodeStr(str) {
   let decoded = "";
+  let i = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    let n = parseInt(str[i]);
+  while (i < str.length) {
+    // Retrieve letter at current index then increment to the num after.
+    let char = str[i++];
+    let numStr = "";
 
-    if (n) {
-      decoded += str[i - 1].repeat(n);
+    /**
+     * Look ahead to find potentially multiple digits after letter.
+     * parseInt returns NaN if it fails to parse to a number.
+     * We can't simply check for a truthy parsed int because 0 is falsy.
+     * We can't use typeof because typeof NaN is "number". This is why we use
+     * not Not a Number to check if we parsed a number.
+     */
+    while (i < str.length && !isNaN(parseInt(str[i]))) {
+      // concatenate the string digit then increment.
+      numStr += str[i++];
     }
+
+    decoded += char.repeat(numStr);
   }
   return decoded;
+}
+
+function decodeStr2(str) {
+  let decoded = "";
+  let numStr = "";
+  let letter = str[0];
+
+  for (let i = 1; i < str.length; i++) {
+    const num = parseInt(str[i]);
+
+    // letter found, means we have passed digits.
+    if (isNaN(num)) {
+      // .repeat parses numStr to int for us.
+      decoded += letter.repeat(numStr);
+      // reset for next letter and it's following digits
+      letter = str[i];
+      numStr = "";
+    } else {
+      numStr += str[i];
+    }
+  }
+  // Since we end with a number concat above doesn't happen for the last one.
+  return decoded + letter.repeat(numStr);
 }
