27c544e2be7ad4171a7591d5583b8808c2aebd04
Neil M <neilm813@gmail.com>
2021-05-11 10:09:58 -0700

    add test cases

--- arrays/diagonalDifference.js
+++ arrays/diagonalDifference.js
@@ -6,16 +6,30 @@
     - top right to bottom left diagonal
 */
 
-const squareMatrix = [
+const squareMatrix1 = [
   [1, 2, 3],
   [4, 5, 6],
   [9, 8, 9],
 ];
-const expected = 2;
+const expected1 = 2;
 /* 
   left to right diagonal: 1 + 5 + 9 = 15
   right to left diagonal: 3 + 5 + 9 = 17
   absolute difference = 2
+*/
+
+const squareMatrix2 = [
+  [1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5],
+];
+const expected2 = 0;
+/* 
+  left to right diagonal: 1 + 2 + 3 + 4 + 5 = 15
+  right to left diagonal: 5 + 4 + 3 + 2 + 1 = 15
+  absolute difference = 0
 */
 
 /**
@@ -67,5 +81,3 @@ const diagonalDifference2 = (sqrMatrix) =>
       0
     )
   );
-
-console.log(diagonalDifference2(squareMatrix));
--- spec/arrays/diagonalDifferenceSpec.js
+++ spec/arrays/diagonalDifferenceSpec.js
@@ -2,14 +2,31 @@ const { diagonalDifference } = require("../../arrays/diagonalDifference");
 const argFormatter = require("../helpers/argFormatter");
 
 describe("diagonalDifference", () => {
-  const squareMatrix = [
+  const squareMatrix1 = [
     [1, 2, 3],
     [4, 5, 6],
     [9, 8, 9],
   ];
-  const expected = 2;
+  const expected1 = 2;
+  /* 
+    left to right diagonal: 1 + 5 + 9 = 15
+    right to left diagonal: 3 + 5 + 9 = 17
+    absolute difference = 2
+  */
+  
+  const squareMatrix2 = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+  ];
+  const expected2 = 0;
 
-  const testCases = [{ arguments: [squareMatrix], expected: expected }];
+  const testCases = [
+    { arguments: [squareMatrix1], expected: expected1 },
+    { arguments: [squareMatrix2], expected: expected2 },
+  ];
 
   it("should return the absolute difference between the sums of the diagonals of a given square matrix.", () =>
     testCases.forEach(({ arguments, expected }) =>
