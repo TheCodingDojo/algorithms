ab853a4e01e10a9ebb9140e61a64f219a4dc260c
Neil M <neilm813@gmail.com>
2021-05-17 09:12:29 -0700

    add test cases

--- recreated_methods/Object/entries.js
+++ recreated_methods/Object/entries.js
@@ -7,15 +7,26 @@
   Do not include key value pairs from the given objects prototype (these are included by default when looping over an object's keys)
 */
 
+const obj1 = {
+  name: "Pizza",
+  calories: 9001,
+};
+
+const expected1 = [
+  ["name", "Pizza"],
+  ["calories", 9001],
+];
+
 const proto = { inheritance: "inherited key", keyOnProto: "val from proto" };
 
-const obj1 = Object.assign(Object.create(proto), {
+// This object contains inherited key value pairs from the above proto obj.
+const obj2 = Object.assign(Object.create(proto), {
   firstName: "Foo",
   lastName: "Bar",
   age: 13,
 });
 
-const expected1 = [
+const expected2 = [
   ["firstName", "Foo"],
   ["lastName", "Bar"],
   ["age", 13],
