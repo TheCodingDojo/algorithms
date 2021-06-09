74c02d561d2305c7f277de5adea85ffa973dc7b8
Neil M <neilm813@gmail.com>
2021-06-09 10:02:29 -0700

    reorder

--- data_structures/SinglyLinkedList.js
+++ data_structures/SinglyLinkedList.js
@@ -208,44 +208,6 @@ class SinglyLinkedList {
   }
 
   /**
-   * Determines whether or not the given search value exists in this list.
-   * - Time: O(n) linear, n = length of list.
-   * - Space: O(1) constant.
-   * @param {any} val The data to search for in the nodes of this list.
-   * @returns {boolean}
-   */
-  contains(val) {
-    let runner = this.head;
-
-    while (runner) {
-      if (runner.data === val) {
-        return true;
-      }
-      runner = runner.next;
-    }
-    return false;
-  }
-
-  /**
-   * Determines whether or not the given search value exists in this list.
-   * - Time: O(n) linear, n = length of list.
-   * - Space: O(1) constant.
-   * @param {any} val The data to search for in the nodes of this list.
-   * @param {?node} current The current node during the traversal of this list
-   *    or null when the end of the list has been reached.
-   * @returns {boolean}
-   */
-  containsRecursive(val, current = this.head) {
-    if (current === null) {
-      return false;
-    }
-    if (current.data === val) {
-      return true;
-    }
-    return this.containsRecursive(val, current.next);
-  }
-
-  /**
    * Removes the last node of this list.
    * - Time: O(n) linear, n = length of list.
    * - Space: O(1) constant.
@@ -302,6 +264,44 @@ class SinglyLinkedList {
       }
     }
     return removedData;
+  }
+
+  /**
+   * Determines whether or not the given search value exists in this list.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @param {any} val The data to search for in the nodes of this list.
+   * @returns {boolean}
+   */
+  contains(val) {
+    let runner = this.head;
+
+    while (runner) {
+      if (runner.data === val) {
+        return true;
+      }
+      runner = runner.next;
+    }
+    return false;
+  }
+
+  /**
+   * Determines whether or not the given search value exists in this list.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @param {any} val The data to search for in the nodes of this list.
+   * @param {?node} current The current node during the traversal of this list
+   *    or null when the end of the list has been reached.
+   * @returns {boolean}
+   */
+  containsRecursive(val, current = this.head) {
+    if (current === null) {
+      return false;
+    }
+    if (current.data === val) {
+      return true;
+    }
+    return this.containsRecursive(val, current.next);
   }
 
   /**
