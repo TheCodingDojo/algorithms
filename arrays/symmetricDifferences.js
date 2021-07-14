2b96da5d6e64da600ff0e5b7ecf45f1858e4e4e0
Neil M <neilm813@gmail.com>
2021-07-14 10:29:32 -0700

    comments

--- arrays/symmetricDifferences.js
+++ arrays/symmetricDifferences.js
@@ -94,27 +94,44 @@ function symmetricDifferencesHashTable(numsA, numsB) {
   const seenB = {};
   const disjunctiveUnion = [];
 
+  // O(n)
   for (const num of numsA) {
     // adding the num as the value avoids having to convert the string key back to int
+    // O(1) - constant can be ignored. This happens n times.
     seenA[num] = num;
   }
 
+  // O(m)
   for (const num of numsB) {
+    // O(1) - constant can be ignored. This happens m times.
     seenB[num] = num;
   }
 
+  // O(n) - could be smaller because of dicts not storing dupes, but if no
+  // dupes it's same length.
   for (const key in seenA) {
+    // O(1)
     if (seenB.hasOwnProperty(key) === false) {
+      // O(1)
       disjunctiveUnion.push(seenA[key]);
     }
   }
 
+  // O(m)
   for (const key in seenB) {
+    // O(1)
     if (seenA.hasOwnProperty(key) === false) {
+      // O(1)
       disjunctiveUnion.push(seenB[key]);
     }
   }
   return disjunctiveUnion;
+  /* 
+  Add all the O notations (multiple for things inside loops) but ignore the
+  constants.
+    O(n) + O(m) + O(n) + O(m) -> O(2n) + O(2m) -> O(2(n + m))
+    Drop the constant 2 and n + m is still linear: O(n) simplified.
+  */
 }
 
 function symmetricDifferencesSets(numsA, numsB) {
