7cbf5bfd7bcf40cab46f66751f2547f688794af9
Neil M <neilm813@gmail.com>
2022-03-22 18:38:20 -0700

    clarified requirements in jsdoc

--- src/strings/encodeStr/index.js
+++ src/strings/encodeStr/index.js
@@ -26,8 +26,8 @@ const expected4 = "bbcc";
 
 /**
  * Encodes the given string such that duplicate characters appear once followed
- * by a number representing how many times the char occurs only if the
- * character occurs more than two time.
+ * by a number representing how many times the char occurs. Only encode strings
+ * when the result yields a shorter length.
  * - Time: O(?).
  * - Space: O(?).
  * @param {string} str The string to encode.
